f9e6f68b54f995aab30e7f998b6f7f7da81b8b80
zafar2199 <zrozmatov0@gmail.com>
2022-05-10 16:33:36 +1000

    GreenSection clear code

--- components/GreenSection/GreenSection.jsx
+++ components/GreenSection/GreenSection.jsx
@@ -3,46 +3,35 @@ import { useSelector } from 'react-redux'
 import { selectLangSlice } from 'store/features/lang'
 
 const GreenSection = ({ data }) => {
-    const { items } = data
     const { lang } = useSelector(selectLangSlice)
+
     return (
         <div className='container'>
-            {items?.map(
-                ({
-                    id,
-                    title,
-                    title_uz,
-                    title_ru,
-                    description,
-                    description_uz,
-                    description_ru,
-                    link,
-                }) => (
-                    <section key={id} className='section-green'>
-                        <h2 className='section-green__title'>
-                            {lang === 'uz'
-                                ? title_uz
-                                : lang === 'ru'
-                                ? title_ru
-                                : title}
-                        </h2>
-                        <p className='section-green__desc'>
-                            {lang === 'uz'
-                                ? description_uz
-                                : lang === 'ru'
-                                ? description_ru
-                                : description}
-                        </p>
-                        <a href={link?.url} className='section-green__btn'>
-                            {lang === 'uz'
-                                ? link?.title_uz
-                                : lang === 'ru'
-                                ? link?.title_ru
-                                : link?.title}
-                        </a>
-                    </section>
-                )
-            )}
+            {data?.items?.map((item) => (
+                <section key={item?.id} className='section-green'>
+                    <h2 className='section-green__title'>
+                        {lang === 'uz'
+                            ? item?.title_uz
+                            : lang === 'ru'
+                            ? item?.title_rugit
+                            : item?.title}
+                    </h2>
+                    <p className='section-green__desc'>
+                        {lang === 'uz'
+                            ? item?.description_uz
+                            : lang === 'ru'
+                            ? item?.description_ru
+                            : item?.description}
+                    </p>
+                    <a href={item?.link?.url} className='section-green__btn'>
+                        {lang === 'uz'
+                            ? item?.link?.title_uz
+                            : lang === 'ru'
+                            ? item?.link?.title_ru
+                            : item?.link?.title}
+                    </a>
+                </section>
+            ))}
         </div>
     )
 }
