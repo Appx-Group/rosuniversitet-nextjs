2481c47fbeedc0d0a53d13920d079a721104a6a8
zafar2199 <zrozmatov0@gmail.com>
2022-05-10 15:40:56 +1000

    Cooperation clear code

--- components/Cooperation/Cooperation.jsx
+++ components/Cooperation/Cooperation.jsx
@@ -13,28 +13,24 @@ import { SircleSvg } from 'assets/icons/Icons'
 
 const Cooperation = ({ data }) => {
     const { lang } = useSelector(selectLangSlice)
-    const {
-        subtitle,
-        subtitle_ru,
-        subtitle_uz,
-        title,
-        title_ru,
-        title_uz,
-        items,
-    } = data
+
     return (
         <div className='container'>
             <SectionTitle
                 title={
-                    lang === 'uz' ? title_uz : lang === 'ru' ? title_ru : title
+                    lang === 'uz'
+                        ? data?.title_uz
+                        : lang === 'ru'
+                        ? data?.title_ru
+                        : data?.title
                 }
                 titleBlue=''
                 subTitle={
                     lang === 'uz'
-                        ? subtitle_uz
+                        ? data?.subtitle_uz
                         : lang === 'ru'
-                        ? subtitle_ru
-                        : subtitle
+                        ? data?.subtitle_ru
+                        : data?.subtitle
                 }
                 theme='white'
                 position='center'
@@ -42,64 +38,51 @@ const Cooperation = ({ data }) => {
             />
             <div className='cooperation '>
                 <VerticalTimeline lineColor='#F7F7F7'>
-                    {items?.map(
-                        (
-                            {
-                                id,
-                                title,
-                                title_uz,
-                                title_ru,
-                                description,
-                                description_uz,
-                                description_ru,
-                            },
-                            index
-                        ) => (
-                            <VerticalTimelineElement
-                                key={id}
-                                position={` ${
-                                    (index + 1) % 2 === 0 ? 'left' : 'right'
-                                }`}
-                                className='vertical-timeline-element--work cooperation__item'
-                                contentStyle={{
-                                    background: '#fff',
-                                    color: '#1b2336',
-                                    boxShadow:
-                                        '11px 10px 38px 0 rgb(46 63 99 / 15%)',
-                                    borderRadius: '8px',
-                                }}
-                                contentArrowStyle={{
-                                    display: 'none',
-                                }}
-                                date={
-                                    lang === 'uz'
-                                        ? title_uz
-                                        : lang === 'ru'
-                                        ? title_ru
-                                        : title
-                                }
-                                dateClassName='cooperation__title'
-                                iconStyle={{
-                                    width: '30px',
-                                    height: '30px',
-                                    background: 'white',
-                                    marginLeft: '-15px',
-                                    transform: 'transleteY(-100%)',
-                                    boxShadow:
-                                        '11px 10px 38px 0 rgb(46 63 99 / 15%)',
-                                }}
-                                icon={<SircleSvg />}
-                            >
-                                <p className='cooperation__desc'>
-                                    {lang === 'uz'
-                                        ? description_uz
-                                        : lang === 'ru'
-                                        ? description_ru
-                                        : description}
-                                </p>
-                            </VerticalTimelineElement>
-                        )
-                    )}
+                    {data?.items?.map((item, index) => (
+                        <VerticalTimelineElement
+                            key={item?.id}
+                            position={` ${
+                                (index + 1) % 2 === 0 ? 'left' : 'right'
+                            }`}
+                            className='vertical-timeline-element--work cooperation__item'
+                            contentStyle={{
+                                background: '#fff',
+                                color: '#1b2336',
+                                boxShadow:
+                                    '11px 10px 38px 0 rgb(46 63 99 / 15%)',
+                                borderRadius: '8px',
+                            }}
+                            contentArrowStyle={{
+                                display: 'none',
+                            }}
+                            date={
+                                lang === 'uz'
+                                    ? item?.title_uz
+                                    : lang === 'ru'
+                                    ? item?.title_ru
+                                    : item?.title
+                            }
+                            dateClassName='cooperation__title'
+                            iconStyle={{
+                                width: '30px',
+                                height: '30px',
+                                background: 'white',
+                                marginLeft: '-15px',
+                                transform: 'transleteY(-100%)',
+                                boxShadow:
+                                    '11px 10px 38px 0 rgb(46 63 99 / 15%)',
+                            }}
+                            icon={<SircleSvg />}
+                        >
+                            <p className='cooperation__desc'>
+                                {lang === 'uz'
+                                    ? item?.description_uz
+                                    : lang === 'ru'
+                                    ? item?.description_ru
+                                    : item?.description}
+                            </p>
+                        </VerticalTimelineElement>
+                    ))}
                 </VerticalTimeline>
             </div>
         </div>
