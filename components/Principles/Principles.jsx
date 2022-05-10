8938c79d1ec45b20c6d832a6799eb2d5e913a93f
zafar2199 <zrozmatov0@gmail.com>
2022-05-10 17:01:51 +1000

    Principles clear code

--- components/Principles/Principles.jsx
+++ components/Principles/Principles.jsx
@@ -7,78 +7,58 @@ import { notImage } from 'assets/data/PartnersDara/ImagesData'
 
 const Principles = ({ data }) => {
     const { lang } = useSelector(selectLangSlice)
-    const {
-        id,
-        subtitle,
-        subtitle_uz,
-        subtitle_ru,
-        title,
-        title_uz,
-        title_ru,
-        items,
-    } = data
+
     return (
         <div className='container'>
             <div className='principles'>
                 <SectionTitle
                     title={
                         lang === 'uz'
-                            ? title_uz
+                            ? data?.title_uz
                             : lang === 'ru'
-                            ? title_ru
-                            : title
+                            ? data?.title_ru
+                            : data?.title
                     }
                     titleBlue={lang === 'uz' ? '' : lang === '' ? '' : ''}
                     subTitle={
                         lang === 'uz'
-                            ? subtitle_uz
+                            ? data?.subtitle_uz
                             : lang === 'ru'
-                            ? subtitle_ru
-                            : subtitle
+                            ? data?.subtitle_ru
+                            : data?.subtitle
                     }
                     theme='white'
                     position='center'
                 />
                 <div className='cart-wrap'>
-                    {items.map(
-                        ({
-                            id,
-                            title,
-                            title_ru,
-                            title_uz,
-                            subtitle,
-                            subtitle_ru,
-                            subtitle_uz,
-                            image,
-                        }) => (
-                            <div key={id} className='cart-about-principles'>
-                                <div className='cart-about-principles__img'>
-                                    <Image
-                                        src={image ? image : notImage}
-                                        alt='about cart img'
-                                        width={200}
-                                        height={180}
-                                    />
-                                </div>
-                                <div className='cart-about-principles__content'>
-                                    <h2 className='cart-about-principles__title'>
-                                        {lang === 'uz'
-                                            ? title_uz
-                                            : lang === 'ru'
-                                            ? title_ru
-                                            : title}
-                                    </h2>
-                                    <p className='cart-about-principles__subtitle'>
-                                        {lang === 'uz'
-                                            ? subtitle_uz
-                                            : lang === 'ru'
-                                            ? subtitle_ru
-                                            : subtitle}
-                                    </p>
-                                </div>
+                    {data?.items.map((item) => (
+                        <div key={id} className='cart-about-principles'>
+                            <div className='cart-about-principles__img'>
+                                <Image
+                                    src={item?.image ? item?.image : notImage}
+                                    alt='about cart img'
+                                    width={200}
+                                    height={180}
+                                />
                             </div>
-                        )
-                    )}
+                            <div className='cart-about-principles__content'>
+                                <h2 className='cart-about-principles__title'>
+                                    {lang === 'uz'
+                                        ? item?.title_uz
+                                        : lang === 'ru'
+                                        ? item?.title_ru
+                                        : item?.title}
+                                </h2>
+                                <p className='cart-about-principles__subtitle'>
+                                    {lang === 'uz'
+                                        ? item?.subtitle_uz
+                                        : lang === 'ru'
+                                        ? item?.subtitle_ru
+                                        : item?.subtitle}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
