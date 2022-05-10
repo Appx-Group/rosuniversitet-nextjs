131d5f1c795b4ad75881f2afbed94a445842091e
zafar2199 <zrozmatov0@gmail.com>
2022-05-10 14:08:41 +1000

    data sucess

--- pages/submit-an-application.js
+++ pages/submit-an-application.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Contacts from '@/components/Contacts/Contacts'
 
-const contacts = ({ data }) => {
-    console.log(data)
+const contacts = ({ data, success }) => {
+    console.log(data, success)
     return <div>{<Contacts data={data} />}</div>
 }
 
@@ -10,11 +10,12 @@ export default contacts
 
 export const getServerSideProps = async () => {
     const res = await fetch(`https://site.bronme.uz/dev/v1/contact`)
-    const { data } = await res.json()
+    const { data, success } = await res.json()
 
     return {
         props: {
             data: data ?? null,
+            success: success,
         },
     }
 }
