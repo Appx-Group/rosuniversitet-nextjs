01c4321703fae04e5f695b05a440c1227d643500
zafar2199 <zrozmatov0@gmail.com>
2022-06-16 17:08:08 +0500

    build project

---
+++ .next/server/chunks/812.js
@@ -0,0 +1,126 @@
+"use strict";
+exports.id = 812;
+exports.ids = [812];
+exports.modules = {
+
+/***/ 9812:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "menu": () => (/* binding */ menu)
+/* harmony export */ });
+const menu = [
+    {
+        id: 1,
+        title: "\u0413\u043B\u0430\u0432\u043D\u0430\u044F",
+        titleUz: "Bosh sahifa",
+        titleEng: "Home",
+        path: "/",
+        subMenu: []
+    },
+    {
+        id: 2,
+        title: "\u041E \u043D\u0430\u0441",
+        titleUz: "Biz haqimizda",
+        titleEng: "About us ",
+        path: "/about-us",
+        subMenu: [
+            {
+                id: 1,
+                title: "\u041E \u043D\u0430\u0441",
+                titleUz: "Biz haqimizda",
+                titleEng: "About us ",
+                path: "/about-us"
+            },
+            {
+                id: 2,
+                title: "\u0424\u0438\u043B\u0438\u0430\u043B\u044B",
+                titleUz: "Filiallar",
+                titleEng: "Branches",
+                path: "/branch"
+            },
+            {
+                id: 3,
+                title: "\u0421\u043E\u0442\u0440\u0443\u0434\u043D\u0438\u0447\u0435\u0441\u0442\u0432\u043E",
+                titleUz: "Hamkorlik",
+                titleEng: "Cooperation",
+                path: "/cooperation"
+            }, 
+        ]
+    },
+    {
+        id: 3,
+        title: "\u041E \u0420\u043E\u0441\u0441\u0438\u0438",
+        titleUz: "Rossiya Haqida",
+        titleEng: "About Russia",
+        path: "/russia",
+        subMenu: [
+            {
+                id: 1,
+                title: "\u041F\u043E\u0447\u0435\u043C\u0443 \u0438\u043C\u0435\u043D\u043D\u043E \u0420\u043E\u0441\u0441\u0438\u0438",
+                titleUz: "Nima uchun Rossiya",
+                titleEng: "Why Russia ",
+                path: "/edu-rus"
+            },
+            {
+                id: 2,
+                title: "\u041E \u0420\u043E\u0441\u0441\u0438\u0438",
+                titleUz: "Rossiya Haqida",
+                titleEng: "About Russia ",
+                path: "/russia"
+            },
+            {
+                id: 3,
+                title: "\u041E\u0431\u0440\u0430\u0437\u043E\u0432\u0430\u043D\u0438\u0435",
+                titleUz: "Ta'lim",
+                titleEng: "Education",
+                path: "/learning"
+            }, 
+        ]
+    },
+    {
+        id: 4,
+        title: "\u0410\u0431\u0438\u0442\u0443\u0440\u0438\u0435\u043D\u0442\u0430\u043C",
+        titleUz: "Abiturientlarga",
+        titleEng: "Applicants",
+        path: "/questions",
+        subMenu: [
+            {
+                id: 1,
+                title: "\u0427\u0430\u0441\u0442\u043E \u0437\u0430\u0434\u0430\u0432\u0430\u0435\u043C\u044B\u0435 \u0432\u043E\u043F\u0440\u043E\u0441\u044B",
+                titleUz: "Tez-tez so'raladigan savollar",
+                titleEng: "Frequently Asked Questions",
+                path: "/questions"
+            },
+            {
+                id: 2,
+                title: "\u0423\u043D\u0438\u0432\u0435\u0440\u0441\u0438\u0442\u0435\u0442\u044B",
+                titleUz: "Universitetlar",
+                titleEng: "Universities",
+                path: "/universities"
+            },
+            {
+                id: 3,
+                title: "\u041A\u0430\u043A \u0441\u0442\u0430\u0442\u044C \u0441\u0442\u0443\u0434\u0435\u043D\u0442\u043E\u043C",
+                titleUz: "Qanday qilib talaba bo'lish kerak?",
+                titleEng: "How to become a student?",
+                path: "/become-student"
+            }, 
+        ]
+    },
+    {
+        id: 5,
+        title: "\u041F\u043E\u0434\u0430\u0442\u044C \u0437\u0430\u044F\u0432\u043A\u0443",
+        titleUz: "Murojat qiling",
+        titleEng: "Submit an application",
+        path: "/contacts",
+        subMenu: []
+    }, 
+];
+
+
+/***/ })
+
+};
+;
--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['site.bronme.uz', 'bronme.uz', 'rosunviversitet.vercel.app'],
+        domains: [
+            'rosunviversitet.vercel.app',
+            'media-app.timeweb-gate.appx.uz',
+        ],
     },
 }
 
