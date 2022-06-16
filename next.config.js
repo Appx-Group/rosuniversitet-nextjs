3a1acdb27e5e657b68e3a599090eec0127127ca3
zafar2199 <zrozmatov0@gmail.com>
2022-06-16 10:02:53 +0500

    change:next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
     images: {
         domains: ['site.bronme.uz', 'bronme.uz', 'rosunviversitet.vercel.app'],
     },
-    experimental: { granularChunks: true },
 }
 
 module.exports = nextConfig
